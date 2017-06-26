e6ff9dcad046d4b7df9c7dcea187139b667cb4e9
phsantiago <pedroh_s_@hotmail.com>
2017-06-26 18:58:43 -0300

    first

---
+++ src/reducers/reducer.js
@@ -0,0 +1,26 @@
+import { ADD_TODO, TOGGLE_TODO } from '../actions'
+
+const initialState = [];
+
+export default function todoApp(state = initialState, action){
+    switch(action.type){
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    done: false
+                }
+            ]
+        break;
+        case TOGGLE_TODO:
+            state.index;                    
+            return state.map( todo => 
+                (todo.id == action.id) ? {...todo, done: !todo.done} : todo
+            )
+        break;
+        default:
+            return state    
+    }
+}
